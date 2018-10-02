96de1ce28dee2f60f40e233297c28c05eb99b9fc
Lucas Cassiano Silva <lucascassiano@users.noreply.github.com>
2018-10-02 00:54:38 -0300

    cpp tests

---
+++ exp/cpp/CMakeFiles/hello-vsc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello-vsc.dir/main.cpp.o"
+  "hello-vsc.pdb"
+  "hello-vsc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello-vsc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ exp/cpp/main.cpp
@@ -0,0 +1,10 @@
+#include "opencv/core.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    cout << "Hello, World!\n\n------";
+    return 0;
+}
